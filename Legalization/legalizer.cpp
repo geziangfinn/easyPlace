28b9496b71d3dc0ca94c43f5577a8fdf082c2414
geziangbuyiding <1041143432@qq.com>
2024-04-01 16:34:09 +0800

    rough implementation of abacus, todo: test and debug

--- Legalization/legalizer.cpp
+++ Legalization/legalizer.cpp
@@ -3,6 +3,75 @@
 void AbacusLegalizer::legalization()
 {
     initialization();
+    for (Module *curCell : dbCells)
+    {
+        double cost = DOUBLE_MAX; // current minimum cost
+        //! assume subrows are sorted by y coordinate(non decreasing)
+        // 1. find closest row
+        int subrowCount = subrows.size();
+        int bestRowIndex = -1;
+        int closestRowIndex = 0;
+        for (AbacusRow curRow : subrows)
+        {
+            if (float_less(fabs(curRow.bottom - curCell->getLL_2D().y), 0.5 * curRow.height)) //? double check here
+            {
+                break;
+            }
+            closestRowIndex++;
+        }
+        //! 2. search in two directions(higher rows and lower rows) for the best row
+        //? optimize here?
+        // first search in higher rows
+        for (int i = closestRowIndex; i < subrowCount; i++)
+        {
+            double costLowerBound = fabs(curCell->getLL_2D().y - subrows[i].bottom); // cost for moving cell in y direction only, //?quadratic or not?
+            // determine cost to move a cell to this row!
+            if (float_greaterorequal(costLowerBound, cost))
+            {
+                break;
+            }
+            double newCost = placeRow(curCell, i, ABACUS_TRIAL);
+            if (float_lessorequal(newCost, cost))
+            {
+                bestRowIndex = i;
+                cost = newCost;
+            }
+        }
+        // then search in lower rows
+        for (int i = closestRowIndex - 1; i >= 0; i--)
+        {
+            double costLowerBound = fabs(curCell->getLL_2D().y - subrows[i].bottom); // cost for moving cell in y direction only
+            if (float_greaterorequal(costLowerBound, cost))
+            {
+                break;
+            }
+            double newCost = placeRow(curCell, i, ABACUS_TRIAL);
+            if (float_lessorequal(newCost, cost))
+            {
+                bestRowIndex = i;
+                cost = newCost;
+            }
+        }
+        // 3. place cell to the best row
+        placeRow(curCell, bestRowIndex, ABACUS_FINAL);
+    }
+
+    //! 4. write cell locations! In placeRow we only determine cluster's best position, without writing cell location
+
+    for (AbacusRow curSubrow : subrows)
+    {
+
+        for (AbacusCellCluster curCluster : curSubrow.clusters)
+        {
+            int x = curCluster.x;
+            for (Module *curCell : curCluster.cells)
+            {
+                placeDB->setModuleLocation_2D(curCell, x, curSubrow.bottom);
+                x += curCell->getWidth();
+                assert(x <= curSubrow.end.x);
+            }
+        }
+    }
 }
 
 void AbacusLegalizer::initialization()
@@ -23,17 +92,38 @@ void AbacusLegalizer::initializeCells()
     }
     sort(dbCells.begin(), dbCells.end(), [=](Module *a, Module *b)
          { return float_lessorequal(a->getLL_2D().x, b->getLL_2D().x); });
-    
+
     //? what's the potential bug when one cell is completely inside another? Should think about it.
 }
 
 void AbacusLegalizer::initializeObstacles()
 {
     //! ignore terminals that are outside the core region
-}
+    //! overlap between macros should be eliminated first!
+    for (Module *curTerminal : placeDB->dbTerminals)
+    {
+        if (float_greater(placeDB->coreRegion.ur.y, curTerminal->getLL_2D().y) && float_less(placeDB->coreRegion.ll.y, curTerminal->getUR_2D().y)) // overlap in y direction
+        {
+            if (float_greater(placeDB->coreRegion.ur.x, curTerminal->getLL_2D().x) && float_less(placeDB->coreRegion.ll.x, curTerminal->getUR_2D().x)) // overlap in x direction
+            {
+                Obstacle newObstacle;
+                newObstacle.ll = curTerminal->getLL_2D();
+                newObstacle.ur = curTerminal->getUR_2D();
+                obstacles.push_back(newObstacle);
+            }
+        }
+    }
 
-void AbacusLegalizer::initializeSubrows()
-{
+    for (Module *curNode : placeDB->dbNodes)
+    {
+        if (curNode->isMacro)
+        {
+            Obstacle newObstacle;
+            newObstacle.ll = curNode->getLL_2D();
+            newObstacle.ur = curNode->getUR_2D();
+            obstacles.push_back(newObstacle);
+        }
+    }
     //! sort obstacles by x coordinate first
     sort(obstacles.begin(), obstacles.end(), [=](Obstacle a, Obstacle b)
          {
@@ -48,11 +138,14 @@ void AbacusLegalizer::initializeSubrows()
             }
             else
             {
-                // terminals overlap!!
-                cerr<<"TERMINALS OVERLAP!\n";
+                // terminals/macros overlap!!
+                cerr<<"TERMINALS/MACROS OVERLAP!\n";
                 exit(0);
             } });
+}
 
+void AbacusLegalizer::initializeSubrows()
+{
     double siteStep = placeDB->dbSiteRows.front().step; //! assume step for all rows are identical!
 
     for (SiteRow curRow : placeDB->dbSiteRows) // generate subrows from dbRows
@@ -87,7 +180,7 @@ void AbacusLegalizer::initializeSubrows()
                 // end.x might be less than start.x when:
                 //    ------
                 //   OOOOO
-                SiteRow newRow;
+                AbacusRow newRow;
                 newRow.start.x = currentX;
                 newRow.start.y = currentRowBottom;
 
@@ -116,7 +209,7 @@ void AbacusLegalizer::initializeSubrows()
         //           OOO
         if (float_less(currentX, curRow.end.x))
         {
-            SiteRow newRow;
+            AbacusRow newRow;
             newRow.start.x = currentX;
             newRow.start.y = currentRowBottom;
 
@@ -159,6 +252,127 @@ void AbacusLegalizer::initializeSubrows()
                 cerr << "sub row new width < 0 when it should not\n";
                 exit(0);
             }
+        }
+    }
+
+    // todo sort subrows by y coordinate!
+}
+
+double AbacusLegalizer::placeRow(Module *cell, int bestRow, bool trial)
+{
+    AbacusRow *tempRowPointer;
+    AbacusRow tempRow;
+    if (trial == ABACUS_TRIAL)
+    {
+        tempRow = subrows[bestRow];
+        tempRowPointer = &tempRow;
+        // for trial place, would not actually modify subrows
+    }
+    else if (trial == ABACUS_FINAL)
+    {
+        tempRowPointer = &subrows[bestRow];
+        // final place
+    }
+    else
+    {
+        cerr << "Not trial nor final\n";
+        exit(0);
+    }
+
+    if (tempRowPointer->clusters.empty() || float_lessorequal(tempRowPointer->clusters.back().x + tempRowPointer->clusters.back().w, cell->getLL_2D().x))
+    {
+        AbacusCellCluster newCluster;
+        newCluster.index = tempRowPointer->clusters.size();                                                //! 0 for the first cluster
+        newCluster.x = int((int(cell->getLL_2D().x + 0.5) / tempRowPointer->step) * tempRowPointer->step); // !! need to make sure that position of clusters align with sites!!!!
+        //? site step: double instead of int, potential precision problems? although step is almost always 1 (never seen any case in which step != 1 so far)
+        //! boundary check
+        if (float_less(newCluster.x, tempRowPointer->start.x))
+        {
+            newCluster.x = tempRowPointer->start.x;
+        }
+        if (float_greater(newCluster.x + cell->getWidth(), tempRowPointer->end.x))
+        {
+            newCluster.x = tempRowPointer->end.x - cell->getWidth();
+        }
+
+        tempRowPointer->clusters.push_back(newCluster);
+        tempRowPointer->addCell(newCluster.index, cell); // addCell: actually always add cell to the last cluster, that is, cluster.back()
+    }
+    else
+    {
+        tempRowPointer->addCell(tempRowPointer->clusters.back().index, cell); // addCell: actually always add cell to the last cluster, that is, cluster.back()
+        tempRowPointer->collapse(tempRowPointer->clusters.back().index);
+    }
+
+    //! operate with tempRowPointer
+    // todo:calculate cell location and return cost!
+    // here, cell must be in the last cluster
+    float x = tempRowPointer->clusters.back().x; // !! need to make sure that position of clusters align with sites!!!! Align in collapse and when creating new clusters
+    for (Module *curCell : tempRowPointer->clusters.back().cells)
+    {
+        if (curCell == cell)
+        {
+            double cost = sqrt((curCell->getLL_2D().x - x) * (curCell->getLL_2D().x - x) + (curCell->getLL_2D().y - tempRowPointer->bottom) * (curCell->getLL_2D().y - tempRowPointer->bottom));
+            return cost;
+            //? sqrt or not?
+        }
+        else
+        {
+            x += curCell->getWidth();
+        }
+    }
+    cerr << "cell not found in cluster??\n";
+    exit(0);
+}
+
+void AbacusRow::addCell(int clusterIndex, Module *cell)
+{
+    assert(clusterIndex < clusters.size());
+    AbacusCellCluster &c = clusters[clusterIndex];
+
+    c.cells.push_back(cell);
+    c.e++;
+    c.q += cell->getLL_2D().x - c.w;
+    c.w += cell->getWidth();
+}
+
+void AbacusRow::addCluster(int predecessorIndex, int clusterIndex)
+{
+    AbacusCellCluster &cPrime = clusters[predecessorIndex];
+    AbacusCellCluster &c = clusters[clusterIndex];
+    cPrime.cells.insert(cPrime.cells.end(), c.cells.begin(), c.cells.end());
+    cPrime.e += c.e;
+    cPrime.q += c.q - c.e * cPrime.w;
+    cPrime.w += c.w;
+}
+
+void AbacusRow::collapse(int clusterIndex)
+{
+    assert(clusterIndex < clusters.size());
+    AbacusCellCluster &c = clusters[clusterIndex];
+
+    // place c
+    c.x = (int)(1.0 * c.q / c.e / step) * step;
+    if (float_less(c.x, start.x))
+    {
+        c.x = start.x;
+    }
+    if (float_greater(c.x + c.w, end.x))
+    {
+        c.x = end.x - c.w;
+    }
+    int predecessorIndex = clusterIndex--;
+    if (predecessorIndex >= 0)
+    {
+        AbacusCellCluster cPrime = clusters[predecessorIndex];
+        if (cPrime.x + cPrime.w > c.x) // actually comparing int here
+        {
+            addCluster(predecessorIndex, clusterIndex);
+
+            assert(clusters[clusterIndex].index == clusterIndex); //!
+
+            clusters.erase(clusters.begin() + clusterIndex);
+            collapse(predecessorIndex);
         }
     }
 }
--- Legalization/legalizer.h
+++ Legalization/legalizer.h
@@ -3,30 +3,54 @@
 #include "global.h"
 #include "objects.h"
 #include "placedb.h"
+#define ABACUS_TRIAL true
+#define ABACUS_FINAL false
 class Obstacle : public CRect
 {
-
 };
-class CellCluster{
+class AbacusCellCluster
+{
+public:
     // cluster of std cells, used in abacus
-	CellCluster()
+    AbacusCellCluster()
     {
         Init();
     }
     void Init()
     {
         cells.clear();
+        index=-1;
+        x=0.0;
+        e=0.0;
+        w=0.0;
+        q=0.0;
     }
-    vector<Module*> cells;//cells should be ordered by x coordinate(non decreasing)
-	int index;
+    vector<Module *> cells; // cells should be ordered by x coordinate(non decreasing)
+    int index;// its index in the vector clusters of class AbacusRow
     // x,e,w,q of a cluster, see the abacus paper
     float x;
-	float e;
-	float w;
-	float q;
+    float e;
+    float w;
+    float q;
 };
+
+class AbacusRow : public SiteRow
+{
+public:
+    AbacusRow()
+    {
+        clusters.clear();
+        //lastClusterIndex=-1;
+    }
+    vector<AbacusCellCluster> clusters;
+    void addCell(int,Module*);
+    void addCluster(int,int);
+    void collapse(int);
+};
+
 class AbacusLegalizer
 {
+public:
     AbacusLegalizer()
     {
         Init();
@@ -41,9 +65,9 @@ class AbacusLegalizer
         placeDB = NULL;
     }
     PlaceDB *placeDB;
-    vector<SiteRow> subrows;  // generated from dbSiteRows and Terminals, this is actually used during legalization
+    vector<AbacusRow> subrows;  // generated from dbSiteRows and Terminals, this is actually used during legalization
     vector<Obstacle> obstacles; // include macro and terminals, macros should be legalized first!
-    vector<Module*> dbCells;// all std cells, sorted by x coordinate(non decreasing order)
+    vector<Module *> dbCells;   // all std cells, sorted by x coordinate(non decreasing order)
     void legalization();
 
 private:
@@ -51,11 +75,13 @@ private:
     // follow the abacus paper
     void initialization();
     void initializeCells();
-    void initializeObstacles();// obstacles include macros and terminals, macros should be legalized first!
+    void initializeObstacles(); // obstacles include macros and terminals, macros should be legalized first!
     void initializeSubrows();
-    void placeRow();
-    void addCell();
-    void addCluster();
-    void collapse();
+    double placeRow(Module *, int, bool);
+};
+
+class MacroLegalizer
+{
+    // todo: build a macro legalizer
 };
 #endif
